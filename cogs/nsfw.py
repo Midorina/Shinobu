d5f7c1eaec4fe2f5a0e1f2528314e41b32a85f3c
Midoriin <fbyigit07@hotmail.com>
2020-05-11 19:34:43 +0300

    rule34 support

--- cogs/nsfw.py
+++ cogs/nsfw.py
@@ -1,3 +1,4 @@
+import json
 import random
 
 import aiohttp
@@ -22,9 +23,15 @@ class NSFW(commands.Cog):
             'flat_chest'
         ]
 
+        self.dapi_links = {
+            'gelbooru': 'https://gelbooru.com/index.php',
+            'rule34': 'https://rule34.xxx/index.php'
+        }
+
         self._cd = commands.CooldownMapping.from_cooldown(rate=2, per=1, type=commands.BucketType.guild)
 
     async def cog_check(self, ctx: Context):
+        print(1)
         bucket = self._cd.get_bucket(ctx.message)
         retry_after = bucket.update_rate_limit()
         if retry_after:  # if on cooldown
@@ -55,30 +62,42 @@ class NSFW(commands.Cog):
 
         return cleaned_tags
 
-    async def get_gelbooru(self, tags) -> str:
+    async def get_nsfw_dapi(self, dapi='rule34', tags=None) -> str:
         tags = self.clean_tags(tags)
         async with aiohttp.ClientSession() as session:
             while True:
-                async with session.get('https://gelbooru.com/index.php', params={
+                async with session.get(self.dapi_links[dapi], params={
                     'page': 'dapi',
                     's': 'post',
                     'q': 'index',
                     'tags': "+".join(tags),
                     'limit': 100,
                     'json': 1,
-                    'pid': random.randrange(200),
+                    'pid': random.randrange(200) if dapi == 'gelbooru' else random.randrange(2000),
                     **self.bot.config['gelbooru_credentials']
                 }) as response:
-                    response_jsond = await response.json()
-                    if not response_jsond:
-                        continue
-                    data = random.choice(list(filter(
-                        lambda x:  x['rating'] != 's' and x['file_url'].split('.')[-1] != '.webm',
-                        response_jsond)))
-                    # print(data.get('file_url'), data.get('rating'), data.get('tags'), sep='\n')
-                    image_url = data.get('file_url')
-                    tags = data.get('tags').split(' ')
+                    if dapi == 'gelbooru':
+                        response_jsond = await response.json()
+                        data = random.choice(list(filter(
+                            lambda x: x['rating'] != 's' and x['file_url'].split('.')[-1] != '.webm',
+                            response_jsond)))
 
+                        image_url = data.get('file_url')
+                        tags = data.get('tags').split(' ')
+
+                    elif dapi == 'rule34':
+                        r = await response.text()
+                        response_jsond = json.loads(r)
+                        filtered = list(filter(
+                            lambda x: x['image'].split('.')[-1] != '.webm',
+                            response_jsond))
+                        if not filtered:
+                            continue
+                        data = random.choice(filtered)
+                        image_url = f"https://img.rule34.xxx/images/{data.get('directory')}/{data.get('image')}"
+                        tags = data.get('tags').split(' ')
+
+                    print(image_url, data.get('rating'), data.get('tags'), sep='\n')
                     # check if it contains a blacklisted tag
                     blacklisted = False
                     for tag in tags:
@@ -90,22 +109,36 @@ class NSFW(commands.Cog):
                     else:
                         return image_url
 
-    @commands.command()
+    @commands.command(aliases=['boob'])
     async def boobs(self, ctx):
+        """Get a random boob picture."""
         e = discord.Embed(color=self.bot.main_color)
         e.set_image(url=await self.get_boobs_or_butts(type='boobs'))
         await ctx.send(embed=e)
 
-    @commands.command()
+    @commands.command(aliases=['butt'])
     async def butts(self, ctx):
+        """Get a random butt picture."""
         e = discord.Embed(color=self.bot.main_color)
         e.set_image(url=await self.get_boobs_or_butts(type='butts'))
         await ctx.send(embed=e)
 
     @commands.command()
     async def gelbooru(self, ctx, *tags):
+        """Get a random image from Gelbooru."""
         e = discord.Embed(color=self.bot.main_color)
-        e.set_image(url=await self.get_gelbooru(tags))
+        e.set_footer(text="Gelbooru")
+        e.set_image(url=await self.get_nsfw_dapi('gelbooru', tags))
+        await ctx.send(embed=e)
+
+    @commands.command()
+    async def rule34(self, ctx, *tags):
+        """Get a random image from Gelbooru."""
+        random_img = await self.get_nsfw_dapi('rule34', tags)
+        e = discord.Embed(color=self.bot.main_color,
+                          description=f"If it doesn't load, click [here]({random_img}).")
+        e.set_footer(text="Rule 34")
+        e.set_image(url=random_img)
         await ctx.send(embed=e)
 
     # TODO: more hentai commands
