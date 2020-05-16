c9febcf20b1e8711011e81f00c8d276ed7f6ff15
Midoriin <fbyigit07@hotmail.com>
2020-05-16 19:10:44 +0300

    added urban search

--- cogs/searches.py
+++ cogs/searches.py
@@ -1,7 +1,8 @@
+import asyncurban
 from discord.ext import commands
 
 from main import MidoBot
-from services import context, base_embed
+from services import context, base_embed, menu_stuff
 from services.google_search import Google
 
 
@@ -9,10 +10,12 @@ class Searches(commands.Cog):
     def __init__(self, bot: MidoBot):
         self.bot = bot
 
-    # async def get_urban_search(self):
+        self.urban = asyncurban.UrbanDictionary(loop=self.bot.loop)
 
     @commands.command(aliases=['g'])
     async def google(self, ctx: context.Context, *, search: str):
+        """Makes a Google search."""
+
         results = await Google().search(query=search)
         e = base_embed.BaseEmbed(self.bot)
 
@@ -23,7 +26,26 @@ class Searches(commands.Cog):
 
         await ctx.send(embed=e)
 
-    # TODO: urban dictionary
+    @commands.command(aliases=['u', 'urbandictionary', 'ud'])
+    async def urban(self, ctx: context.Context, *, search: str):
+        """Searches the definition of a word on UrbanDictionary."""
+
+        try:
+            word_list = await self.urban.search(search, limit=5)
+        except asyncurban.WordNotFoundError:
+            return await ctx.send("Could not find any definition.")
+        blocks = list()
+
+        e = base_embed.BaseEmbed(self.bot)
+        for word in word_list:
+            base = f"**[{word.word}]({word.permalink})**\n\n{word.definition.replace('[', '**').replace(']', '**')}"
+
+            if word.example:
+                base += f"\n\n**Example:**\n\n{word.example.replace('[', '**').replace(']', '**')}"
+
+            blocks.append(base)
+
+        await menu_stuff.paginate(self.bot, ctx, embed=e, blocks=blocks, item_per_page=1)
 
 
 def setup(bot):
