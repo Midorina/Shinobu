55044b98aac79ca40690e39f75a2d0d2d1dc2d78
Midoriin <fbyigit07@hotmail.com>
2020-05-16 16:46:36 +0300

    improved nsfw API searches, NSFW cog cooldown

--- cogs/nsfw.py
+++ cogs/nsfw.py
@@ -9,6 +9,10 @@ from main import MidoBot
 from services.context import Context
 
 
+class NotFoundError(Exception):
+    pass
+
+
 class NSFW(commands.Cog):
     def __init__(self, bot: MidoBot):
         self.bot = bot
@@ -31,11 +35,10 @@ class NSFW(commands.Cog):
         self._cd = commands.CooldownMapping.from_cooldown(rate=2, per=1, type=commands.BucketType.guild)
 
     async def cog_check(self, ctx: Context):
-        print(1)
         bucket = self._cd.get_bucket(ctx.message)
         retry_after = bucket.update_rate_limit()
         if retry_after:  # if on cooldown
-            return False
+            raise commands.CommandOnCooldown(bucket, retry_after)
 
         if not isinstance(ctx.channel, discord.DMChannel) and not ctx.channel.is_nsfw():
             await ctx.send('This command can only be used in channels that are marked as NSFW.')
@@ -44,12 +47,12 @@ class NSFW(commands.Cog):
         return True
 
     @staticmethod
-    async def get_boobs_or_butts(type='boobs') -> str:
+    async def get_boobs_or_butts(_type='boobs') -> str:
         async with aiohttp.ClientSession() as session:
-            async with session.get(f'http://api.o{type}.ru/noise/1/') as r:
+            async with session.get(f'http://api.o{_type}.ru/noise/1/') as r:
                 if r.status == 200:
                     data = await r.json()
-                    return f"http://media.o{type}.ru/" + data[0]['preview']
+                    return f"http://media.o{_type}.ru/" + data[0]['preview']
                 else:
                     raise Exception('Couldn\t fetch image. Please try again later.')
 
@@ -64,8 +67,12 @@ class NSFW(commands.Cog):
 
     async def get_nsfw_dapi(self, dapi='rule34', tags=None) -> str:
         tags = self.clean_tags(tags)
+        max_range = 200 if dapi == 'gelbooru' else 2000
+
         async with aiohttp.ClientSession() as session:
             while True:
+                rand_page = random.randrange(max_range) if max_range else 0
+
                 async with session.get(self.dapi_links[dapi], params={
                     'page': 'dapi',
                     's': 'post',
@@ -73,37 +80,61 @@ class NSFW(commands.Cog):
                     'tags': "+".join(tags),
                     'limit': 100,
                     'json': 1,
-                    'pid': random.randrange(200) if dapi == 'gelbooru' else random.randrange(2000),
-                    **self.bot.config['gelbooru_credentials']
+                    'pid': rand_page,
+                    # **self.bot.config['gelbooru_credentials']
                 }) as response:
+                    # lower the range
+                    max_range = rand_page
+
                     if dapi == 'gelbooru':
-                        response_jsond = await response.json()
-                        data = random.choice(list(filter(
-                            lambda x: x['rating'] != 's' and x['file_url'].split('.')[-1] != '.webm',
-                            response_jsond)))
+                        response_jsond = await response.json() or []
+                        filtered = list(filter(
+                            lambda x: x['rating'] != 's' and not x['file_url'].endswith('.webm'),
+                            response_jsond))
+
+                        if not filtered:
+                            # if we're at the last page
+                            if rand_page == 0:
+                                # print(response_jsond)
+                                print(rand_page)
+                                print(response.url)
+                                raise NotFoundError
+                            else:
+                                continue
+
+                        data = random.choice(filtered)
 
                         image_url = data.get('file_url')
-                        tags = data.get('tags').split(' ')
+                        image_tags = data.get('tags').split(' ')
 
                     elif dapi == 'rule34':
                         r = await response.text()
+
                         response_jsond = json.loads(r)
                         filtered = list(filter(
-                            lambda x: x['image'].split('.')[-1] != '.webm',
+                            lambda x: not x['image'].endswith('.webm'),
                             response_jsond))
+
                         if not filtered:
-                            continue
+                            # if we're at the last page
+                            if rand_page == 0:
+                                raise NotFoundError
+                            else:
+                                continue
+
                         data = random.choice(filtered)
                         image_url = f"https://img.rule34.xxx/images/{data.get('directory')}/{data.get('image')}"
-                        tags = data.get('tags').split(' ')
+                        image_tags = data.get('tags').split(' ')
 
-                    print(image_url, data.get('rating'), data.get('tags'), sep='\n')
+                    print(image_url, image_tags, sep='\n')
+
                     # check if it contains a blacklisted tag
                     blacklisted = False
-                    for tag in tags:
+                    for tag in image_tags:
                         if tag in self.blacklisted_tags:
                             blacklisted = True
                             break
+
                     if blacklisted:
                         continue
                     else:
@@ -113,28 +144,37 @@ class NSFW(commands.Cog):
     async def boobs(self, ctx):
         """Get a random boob picture."""
         e = discord.Embed(color=self.bot.main_color)
-        e.set_image(url=await self.get_boobs_or_butts(type='boobs'))
+        e.set_image(url=await self.get_boobs_or_butts(_type='boobs'))
         await ctx.send(embed=e)
 
     @commands.command(aliases=['butt'])
     async def butts(self, ctx):
         """Get a random butt picture."""
         e = discord.Embed(color=self.bot.main_color)
-        e.set_image(url=await self.get_boobs_or_butts(type='butts'))
+        e.set_image(url=await self.get_boobs_or_butts(_type='butts'))
         await ctx.send(embed=e)
 
     @commands.command()
     async def gelbooru(self, ctx, *tags):
         """Get a random image from Gelbooru."""
+        try:
+            random_img = await self.get_nsfw_dapi('gelbooru', tags)
+        except NotFoundError:
+            return await ctx.send("No results.")
+
         e = discord.Embed(color=self.bot.main_color)
         e.set_footer(text="Gelbooru")
-        e.set_image(url=await self.get_nsfw_dapi('gelbooru', tags))
+        e.set_image(url=random_img)
         await ctx.send(embed=e)
 
     @commands.command()
     async def rule34(self, ctx, *tags):
         """Get a random image from Gelbooru."""
-        random_img = await self.get_nsfw_dapi('rule34', tags)
+        try:
+            random_img = await self.get_nsfw_dapi('rule34', tags)
+        except NotFoundError:
+            return await ctx.send("No results.")
+
         e = discord.Embed(color=self.bot.main_color,
                           description=f"If it doesn't load, click [here]({random_img}).")
         e.set_footer(text="Rule 34")
