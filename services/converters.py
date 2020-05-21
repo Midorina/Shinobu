0af28bc1627f0bfb4c14a05cecd36bea3b0dc015
Midoriin <fbyigit07@hotmail.com>
2020-05-21 13:32:31 +0300

    improved member converter for the moderation module

--- services/converters.py
+++ services/converters.py
@@ -7,8 +7,14 @@ class BetterMemberconverter(commands.MemberConverter):
         try:
             member = await super().convert(ctx, argument)
         except commands.BadArgument:
-            member = discord.utils.find(lambda m: m.name.lower() == argument.lower(), ctx.guild.members)
-            if not member:
-                raise commands.BadArgument(f"Member \"{argument}\" not found.")
+            # if we cant find with id, create miniature discord obj
+            if argument.isdigit():
+                member = discord.Object(id=int(argument))
+
+            # if its a string
+            else:
+                member = discord.utils.find(lambda m: m.name.lower() == argument.lower(), ctx.guild.members)
+                if not member:
+                    raise commands.BadArgument(f"Member \"{argument}\" not found.")
 
         return member
