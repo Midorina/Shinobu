266717af1cd3246bcc95e59e1b7c200b8094cb1b
Midoriin <fbyigit07@hotmail.com>
2020-05-26 07:39:59 +0300

    changes to the base embed

--- services/base_embed.py
+++ services/base_embed.py
@@ -4,13 +4,13 @@ import discord
 
 
 class BaseEmbed(discord.Embed):
-    def __init__(self, bot, **kwargs):
+    def __init__(self, bot, footer=True, **kwargs):
         super().__init__(**kwargs)
         self.bot = bot
 
         self.color = self.color if self.color else 0x15a34a
 
-        if not hasattr(self, '_footer'):
+        if footer is True and not hasattr(self, '_footer'):
             self.set_footer(text=self.bot.user.name, icon_url=self.bot.user.avatar_url)
 
-        self.timestamp = self.timestamp if self.timestamp else datetime.utcnow()
+            self.timestamp = self.timestamp if self.timestamp else datetime.utcnow()
