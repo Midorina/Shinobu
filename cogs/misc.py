bd79b82761aee8eaba7d7dae8f30aa06f0b64776
Midoriin <fbyigit07@hotmail.com>
2020-06-16 01:51:11 +0300

    removed prefix check for the help command

--- cogs/misc.py
+++ cogs/misc.py
@@ -21,55 +21,6 @@ class MidoHelp(commands.HelpCommand):
         })
         self.verify_checks = False
 
-    # overriding this for additional prefix check
-    async def command_callback(self, ctx, *, command=None):
-        await self.prepare_help_command(ctx, command)
-        bot = ctx.bot
-
-        if command is None:
-            mapping = self.get_bot_mapping()
-            return await self.send_bot_help(mapping)
-
-        # Check if it's a cog
-        cog = bot.get_cog(command)
-        if cog is not None:
-            return await self.send_cog_help(cog)
-
-        maybe_coro = discord.utils.maybe_coroutine
-
-        # If it's not a cog then it's a command.
-        # Since we want to have detailed errors when someone
-        # passes an invalid subcommand, we need to walk through
-        # the command group chain ourselves.
-        keys = command.split(' ')
-
-        # maybe it has a prefix at the beginning
-        prefix_length = len(self.context.prefix)
-        if keys[0][:prefix_length] == self.context.prefix:
-            keys[0] = keys[0][prefix_length:]
-
-        cmd = bot.all_commands.get(keys[0])
-        if cmd is None:
-            string = await maybe_coro(self.command_not_found, self.remove_mentions(keys[0]))
-            return await self.send_error_message(string)
-
-        for key in keys[1:]:
-            try:
-                found = cmd.all_commands.get(key)
-            except AttributeError:
-                string = await maybe_coro(self.subcommand_not_found, cmd, self.remove_mentions(key))
-                return await self.send_error_message(string)
-            else:
-                if found is None:
-                    string = await maybe_coro(self.subcommand_not_found, cmd, self.remove_mentions(key))
-                    return await self.send_error_message(string)
-                cmd = found
-
-        if isinstance(cmd, commands.Group):
-            return await self.send_group_help(cmd)
-        else:
-            return await self.send_command_help(cmd)
-
     def command_not_found(self, string):
         return f'Couldn\'t find any command called `{string}`'
 
