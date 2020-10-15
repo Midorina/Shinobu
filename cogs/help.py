a83ffbcc783248bc8641f35992f36354b18c369e
Midoriin <fbyigit07@hotmail.com>
2020-10-15 21:56:09 +0300

    moved the help to a different cog

---
+++ cogs/help.py
@@ -0,0 +1,102 @@
+from datetime import datetime
+
+from discord.ext import commands
+
+from midobot import MidoBot
+from services.context import MidoContext
+from services.embed import MidoEmbed
+
+
+class MidoHelp(commands.HelpCommand):
+    def __init__(self):
+        super().__init__(command_attrs={
+            'cooldown': commands.Cooldown(rate=1, per=1.0, type=commands.BucketType.member),
+            'help'    : 'Shows help about the bot or a command.',
+            'aliases' : ['h']
+        })
+        self.verify_checks = False
+
+    def command_not_found(self, string):
+        return f'Couldn\'t find any command called `{string}`'
+
+    async def on_help_command_error(self, ctx: MidoContext, error):
+        if isinstance(error, commands.CommandInvokeError):
+            await ctx.send(str(error.original))
+
+    def get_command_signature(self, command):
+        parent = command.full_parent_name
+
+        if len(command.aliases) > 0:
+            aliases = '|'.join(command.aliases)
+            fmt = f'[{command.name}|{aliases}]'
+            if parent:
+                fmt = f'{parent} {fmt}'
+            alias = fmt
+        else:
+            alias = command.name if not parent else f'{parent} {command.name}'
+
+        return f'{alias} {command.signature}'
+
+    async def send_bot_help(self, mapping):
+        import itertools
+
+        def key(c):
+            return c.cog_name or '\u200bNo Category'
+
+        entries = await self.filter_commands(self.context.bot.commands, sort=True, key=key)
+        total = 0
+
+        e = MidoEmbed(self.context.bot,
+                      title='MidoBot Commands',
+                      description=f'You can type `{self.context.prefix}help <command>` '
+                                  f'to see additional info about a command.',
+                      default_footer=True)
+        for cog, _commands in itertools.groupby(entries, key=key):
+            _commands = sorted(_commands, key=lambda c: c.name)
+            if len(_commands) == 0:
+                continue
+
+            total += len(_commands)
+            e.add_field(name=f"**__{str(cog)}__**",
+                        value="\n".join([f'{self.context.prefix}**{c.name}**' for c in _commands]),
+                        inline=True)
+
+        e.set_footer(text=f"{total} Commands",
+                     icon_url=self.context.bot.user.avatar_url)
+        e.timestamp = datetime.utcnow()
+
+        await self.context.send(embed=e)
+
+    def common_command_formatting(self, embed, command):
+        embed.title = self.context.prefix + self.get_command_signature(command)
+        if command.description:
+            embed.description = f'{command.description}\n\n{command.help}'
+        else:
+            if not command.help:
+                embed.description = 'There\'s no help information about this command...'
+            else:
+                embed.description = command.help.format(self.context)
+
+    async def send_command_help(self, command, content=''):
+        if command.hidden:
+            raise commands.CommandInvokeError("That is a hidden command. Sorry.")
+
+        embed = MidoEmbed(self.context.bot, default_footer=True)
+        self.common_command_formatting(embed, command)
+        await self.context.send(content=content, embed=embed)
+
+
+class HelpCog(commands.Cog):
+    def __init__(self, bot: MidoBot):
+        self.bot = bot
+
+        self.old_help_command = bot.help_command
+        bot.help_command = MidoHelp()
+        bot.help_command.cog = self
+
+    def cog_unload(self):
+        self.bot.help_command = self.old_help_command
+
+
+def setup(bot):
+    bot.add_cog(HelpCog(bot))
--- cogs/misc.py
+++ cogs/misc.py
@@ -1,9 +1,7 @@
 import ast
-import itertools
 import multiprocessing
 import os
 import time
-from datetime import datetime
 
 import discord
 import psutil
@@ -17,83 +15,6 @@ from services.embed import MidoEmbed
 from services.exceptions import EmbedError
 from services.resources import Resources
 from services.time_stuff import MidoTime
-
-
-class MidoHelp(commands.HelpCommand):
-    def __init__(self):
-        super().__init__(command_attrs={
-            'cooldown': commands.Cooldown(rate=1, per=1.0, type=commands.BucketType.member),
-            'help'    : 'Shows help about the bot or a command.',
-            'aliases' : ['h']
-        })
-        self.verify_checks = False
-
-    def command_not_found(self, string):
-        return f'Couldn\'t find any command called `{string}`'
-
-    async def on_help_command_error(self, ctx: MidoContext, error):
-        if isinstance(error, commands.CommandInvokeError):
-            await ctx.send(str(error.original))
-
-    def get_command_signature(self, command):
-        parent = command.full_parent_name
-
-        if len(command.aliases) > 0:
-            aliases = '|'.join(command.aliases)
-            fmt = f'[{command.name}|{aliases}]'
-            if parent:
-                fmt = f'{parent} {fmt}'
-            alias = fmt
-        else:
-            alias = command.name if not parent else f'{parent} {command.name}'
-
-        return f'{alias} {command.signature}'
-
-    async def send_bot_help(self, mapping):
-        def key(c):
-            return c.cog_name or '\u200bNo Category'
-
-        entries = await self.filter_commands(self.context.bot.commands, sort=True, key=key)
-        total = 0
-
-        e = MidoEmbed(self.context.bot,
-                      title='MidoBot Commands',
-                      description=f'You can type `{self.context.prefix}help <command>` '
-                                  f'to see additional info about a command.',
-                      default_footer=True)
-        for cog, _commands in itertools.groupby(entries, key=key):
-            _commands = sorted(_commands, key=lambda c: c.name)
-            if len(_commands) == 0:
-                continue
-
-            total += len(_commands)
-            e.add_field(name=f"**__{str(cog)}__**",
-                        value="\n".join([f'{self.context.prefix}**{c.name}**' for c in _commands]),
-                        inline=True)
-
-        e.set_footer(text=f"{total} Commands",
-                     icon_url=self.context.bot.user.avatar_url)
-        e.timestamp = datetime.utcnow()
-
-        await self.context.send(embed=e)
-
-    def common_command_formatting(self, embed, command):
-        embed.title = self.context.prefix + self.get_command_signature(command)
-        if command.description:
-            embed.description = f'{command.description}\n\n{command.help}'
-        else:
-            if not command.help:
-                embed.description = 'There\'s no help information about this command...'
-            else:
-                embed.description = command.help.format(self.context)
-
-    async def send_command_help(self, command, content=''):
-        if command.hidden:
-            raise commands.CommandInvokeError("That is a hidden command. Sorry.")
-
-        embed = MidoEmbed(self.context.bot, default_footer=True)
-        self.common_command_formatting(embed, command)
-        await self.context.send(content=content, embed=embed)
 
 
 def insert_returns(body):
@@ -115,15 +36,9 @@ def insert_returns(body):
 class Misc(commands.Cog):
     def __init__(self, bot: MidoBot):
         self.bot = bot
-        self.old_help_command = bot.help_command
-        bot.help_command = MidoHelp()
-        bot.help_command.cog = self
 
         self.process = psutil.Process(os.getpid())
         self.update_name_cache.start()
-
-    def cog_unload(self):
-        self.bot.help_command = self.old_help_command
 
     @tasks.loop(minutes=10)
     async def update_name_cache(self):
