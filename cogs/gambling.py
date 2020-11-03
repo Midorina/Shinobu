dee475aaf53fed2791ab2539736b8fb5b06cf28e
Midoriin <fbyigit07@hotmail.com>
2020-11-03 19:26:50 +0300

    wheel command

--- cogs/gambling.py
+++ cogs/gambling.py
@@ -7,8 +7,10 @@ from discord.ext import commands
 
 from midobot import MidoBot
 from models.db_models import UserDB
-from services import checks, context
+from services import checks
+from services.context import MidoContext
 from services.converters import MidoMemberConverter
+from services.embed import MidoEmbed
 from services.exceptions import EmbedError
 from services.resources import Resources
 
@@ -67,7 +69,7 @@ class Gambling(commands.Cog):
                              '{}'.format(data))
 
     @commands.command(aliases=['$', 'money'])
-    async def cash(self, ctx: context.MidoContext, *, user: MidoMemberConverter() = None):
+    async def cash(self, ctx: MidoContext, *, user: MidoMemberConverter() = None):
         """Check the cash status of you or someone else."""
         if user:
             user_db = await UserDB.get_or_create(ctx.db, user.id)
@@ -78,7 +80,7 @@ class Gambling(commands.Cog):
         await ctx.send_success(f"**{user.mention}** has **{user_db.cash}{Resources.emotes.currency}**!")
 
     @commands.command()
-    async def daily(self, ctx: context.MidoContext):
+    async def daily(self, ctx: MidoContext):
         """
         Claim {0.bot.config[daily_amount]}{0.resources.emotes.currency} for free every 12 hours by upvoting [here]({0.resources.links.upvote}).
         """
@@ -102,7 +104,7 @@ class Gambling(commands.Cog):
                                    f"your daily **{daily_amount}{Resources.emotes.currency}**!")
 
     @commands.command(name="flip", aliases=['cf', 'coinflip'])
-    async def coin_flip(self, ctx: context.MidoContext, amount: Union[int, str], guessed_side: str):
+    async def coin_flip(self, ctx: MidoContext, amount: Union[int, str], guessed_side: str):
         """A coin flip game. You'll earn the double amount of what you bet if you predict correctly.
 
         Sides and Aliases:
@@ -137,9 +139,48 @@ class Gambling(commands.Cog):
 
         return await ctx.send(embed=e)
 
+    @commands.command()
+    async def wheel(self, ctx: MidoContext, amount: Union[int, str]):
+        """Turn the wheel!"""
+        possibilities_and_arrows = {
+            1.5: '↖️',
+            1.7: '⬆️',
+            2.4: '↗️',
+            0.2: '⬅️',
+            1.2: '➡️',
+            0.1: '↙️',
+            0.3: '⬇️',
+            0.5: '↘️'
+        }
+        empty = "⠀"
+
+        e = MidoEmbed(bot=self.bot)
+
+        won_multiplier, won_arrow = random.choice(list(possibilities_and_arrows.items()))
+        won_cash = int(won_multiplier * amount)
+
+        e.set_author(icon_url=ctx.author.avatar_url,
+                     name=f'{ctx.author.display_name} has just won: {won_cash}{Resources.emotes.currency}')
+
+        await ctx.user_db.add_cash(won_cash)
+
+        e.description = ""
+        for i, multiplier_and_arrow in enumerate(possibilities_and_arrows.items()):
+            multiplier, arrow = multiplier_and_arrow
+
+            if i == 4:
+                e.description += empty + won_arrow + empty * 7
+
+            e.description += f'**『{multiplier}』**{empty * 5}'
+
+            if i in (2, 4):
+                e.description += '\n\n'
+
+        await ctx.send(embed=e)
+
     @commands.command(name="give")
     @commands.guild_only()
-    async def give_cash(self, ctx: context.MidoContext, amount: Union[int, str], *, member: MidoMemberConverter()):
+    async def give_cash(self, ctx: MidoContext, amount: Union[int, str], *, member: MidoMemberConverter()):
         """Give a specific amount of cash to someone else."""
         if member.id == ctx.author.id:
             raise EmbedError("Why'd you send money to yourself?")
@@ -152,7 +193,7 @@ class Gambling(commands.Cog):
 
     @checks.owner_only()
     @commands.command(name="award", hidden=True)
-    async def add_cash(self, ctx: context.MidoContext, amount: Union[int, str], *, member: MidoMemberConverter()):
+    async def add_cash(self, ctx: MidoContext, amount: Union[int, str], *, member: MidoMemberConverter()):
         other_usr = await UserDB.get_or_create(ctx.db, member.id)
 
         await other_usr.add_cash(amount)
@@ -161,15 +202,16 @@ class Gambling(commands.Cog):
 
     @checks.owner_only()
     @commands.command(name="punish", aliases=['withdraw'], hidden=True)
-    async def remove_cash(self, ctx: context.MidoContext, amount: Union[int, str], *, member: MidoMemberConverter()):
+    async def remove_cash(self, ctx: MidoContext, amount: Union[int, str], *, member: MidoMemberConverter()):
         other_usr = await UserDB.get_or_create(ctx.db, member.id)
 
         await other_usr.remove_cash(amount)
         await ctx.send_success(f"You've just removed **{amount}{Resources.emotes.currency}** from {member}.")
 
+    @wheel.before_invoke
     @coin_flip.before_invoke
     @give_cash.before_invoke
-    async def ensure_not_broke(self, ctx: context.MidoContext):
+    async def ensure_not_broke(self, ctx: MidoContext):
         if ctx.user_db.cash <= 0:
             raise EmbedError("You don't have any money.")
 
