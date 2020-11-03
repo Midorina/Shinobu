caa93c6c69b8ee233c5a7426a507213e38dec5aa
Midoriin <fbyigit07@hotmail.com>
2020-11-03 15:40:25 +0300

    8ball

--- cogs/shitposting.py
+++ cogs/shitposting.py
@@ -5,16 +5,39 @@ import discord
 from discord.ext import commands
 
 from midobot import MidoBot
+from services.context import MidoContext
 from services.converters import MidoMemberConverter
 from services.embed import MidoEmbed
+from services.resources import Resources
 
 
 class Shitposting(commands.Cog):
     def __init__(self, bot: MidoBot):
         self.bot = bot
 
+    @commands.command(name='8ball')
+    async def _8ball(self, ctx: MidoContext, *, question: str):
+        """Ask a question to 8ball."""
+        answer_index = random.randint(0, 19)
+
+        e = MidoEmbed(bot=self.bot)
+        e.set_author(icon_url=ctx.author.avatar_url, name=ctx.author)
+
+        e.add_field(name='❓ Question', value=question, inline=False)
+        e.add_field(name='🎱 8ball', value=Resources.strings.eight_ball_responses[answer_index], inline=False)
+
+        if answer_index < 10:
+            e.colour = 0x008000
+        elif answer_index < 15:
+            e.colour = 0xffd700
+        else:
+            e.colour = 0xff0000
+
+        await ctx.send(embed=e)
+
     @commands.command()
     async def penis(self, ctx, *, target: Union[MidoMemberConverter, str] = None):
+        """Learn the size of penis of someone."""
         user = target or ctx.author
         if isinstance(user, discord.Member):
             user = user.display_name
@@ -28,6 +51,7 @@ class Shitposting(commands.Cog):
 
     @commands.command()
     async def howgay(self, ctx, *, target: Union[MidoMemberConverter, str] = None):
+        """Learn how gay someone is."""
         user = target or ctx.author
         if isinstance(user, discord.Member):
             user = user.display_name
