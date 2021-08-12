a1298bbc989dcc5a1fabd6c74f594f7ad21b6d25
Midoriin <fbyigit07@hotmail.com>
2021-08-12 18:05:43 +0300

    cluster stats bug fix

--- ipc/ipc_funcs.py
+++ ipc/ipc_funcs.py
@@ -298,7 +298,7 @@ class IPCServer:
             "memory"       : self.process.memory_info().rss / 10 ** 6,
             "threads"      : self.process.num_threads(),
             "cpu_usage"    : self.cpu_usage_cache,
-            "music_players": len(self.bot.wavelink.players)
+            "music_players": len(self.bot.wavelink.players) if hasattr(self.bot, 'wavelink') else 0
         }
 
     async def get_patron(self, data: IPCMessage):
