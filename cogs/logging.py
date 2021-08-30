1d878d1d7b12c6f3ee722c058c536d5167c1cb13
Midoriin <fbyigit07@hotmail.com>
2021-08-30 22:23:24 +0300

    limit content in log message to avoid HTTPException

--- cogs/logging.py
+++ cogs/logging.py
@@ -291,7 +291,7 @@ class Logging(
                 content = f"{time} :x: {len(msgs)} messages have been deleted in {self.detailed(channel)}."
 
         try:
-            await self.bot.send_as_webhook(guild_settings.logging_channel, content=content,
+            await self.bot.send_as_webhook(guild_settings.logging_channel, content=content[:2000],
                                            embed=e,
                                            file=file,
                                            allowed_mentions=discord.AllowedMentions.none())
